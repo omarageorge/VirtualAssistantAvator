96b2892b2fc5038744db7d11dd44c691b31e6451
georgie105 <georgeomara8@gmail.com>
2021-04-04 22:48:48 +0300

    Added a few commands to the commandEngine

--- README.md
+++ README.md
@@ -1,29 +1,34 @@
 # VirtualAssistantAvator
-A Python Virtual Assistant Project 
+
+A Python Virtual Assistant Project
+
 ### Requirements
+
 - Python3
 - Virtual environment(python3 venv)
 - git
 
-
 ### Clone repository
+
 ```
 git clone https://github.com/georgie105/VirtualAssistantAvator.git
 ```
 
-
 ### Setup a virtual environment
+
 The Virtual environment is useful for isolating your project files such that any pip3 installations
 are only installed to the project and not globally to your computer.
+
 ```
 python3 -m venv venv
 source venv/bin/activate
 ```
 
-
 ### External Library Installations
-1. SpeechRecognition: `pip3 install SpeechRecognition`  -> Library for performing speech recognition
-2. PyAudio: `pip3 install PyAudio`  -> required for using microphone input
-3. gTTS: `pip3 install gTTS`  -> CLI tool to interface with Google Translate's text-to-speech
-4. playsound `pip3 install playsound`  -> For playing sounds
 
+1. SpeechRecognition: `pip3 install SpeechRecognition` -> Library for performing speech recognition
+2. PyAudio: `pip install PyAudio` -> required for using microphone input
+3. gTTS: `pip install gTTS` -> CLI tool to interface with Google Translate's text-to-speech
+4. playsound `pip install playsound` -> For playing sounds
+5. python-weather `pip install python-weather` -> A free and asynchronous weather API wrapper
+6. wikipedia `pip install wikipedia` -> A library that makes it easy to access and parse data from Wikipedia
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
+import asyncio
 import speech_recognition as sr
 from gtts import gTTS
 import playsound as pl
 import os
-import datetime
+from datetime import date, datetime, time
+import python_weather
 import warnings
-import calendar
-import random
 import wikipedia
 
 
@@ -14,7 +14,7 @@ warnings.filterwarnings('ignore')
 
 
 # Listen to commands
-def listener():
+def soundEngine():
     
     # Create a listener object
     listener = sr.Recognizer()
@@ -28,6 +28,9 @@ def listener():
         # Store data received from microphone
         microphone_data = listener.listen(source)
         
+        # Covert to lowercase
+        microphone_data
+        
         # Stores text data from conversion
         sound_to_text = ''
         
@@ -35,21 +38,17 @@ def listener():
             # Convert microphone data to text
             sound_to_text = listener.recognize_google(microphone_data)
         except:
-            print('Sorry, I did not get that!')
+            pass
         
-        return sound_to_text
+        return sound_to_text.lower()
         
         
 
-# Speech
+# Speech Handler
 def speechEngine(text):
     
     # Sound file name
     sound_file = 'voice_audio_file.mp3'
-    
-    # Check is Voice audio file already exists
-    if os.path.isfile(sound_file):
-        os.remove(sound_file)
     
     # Convert text to speech
     soundObject = gTTS(text, lang='en', slow=False)
@@ -60,3 +59,73 @@ def speechEngine(text):
     # Talk
     pl.playsound(sound_file)
     
+    # Cleanup
+    if os.path.isfile(sound_file):
+        os.remove(sound_file)
+    
+    
+# Handles commands passed into the system
+def commandEngine():
+    while True:
+            
+        # Load output from microphone
+        command = soundEngine()
+        
+        # command = 'gye'
+        
+        
+    
+        if 'goodbye' in command:     # Exit Handler
+            speechEngine('Have a nice time, Goodbye!')
+            break
+        
+        elif 'date' in command: # Date Handler
+            today = date.today()
+            date_response = f"The date today is {today.strftime('%d %B, %Y')}."
+            speechEngine(date_response)
+            
+        elif 'time' in command: # Time handler
+            time_str = datetime.now()
+            time_response = f"The time is {time_str.strftime('%I:%M %p')}."
+            speechEngine(time_response)
+            
+        elif 'weather' in command:  # Weather Handler
+            # set location
+            location = 'greenland'
+            
+            # Weather finder
+            async def getWeather(location):
+                # Declare the client
+                client = python_weather.Client(format=python_weather.IMPERIAL)
+                weather =  await client.find(location)
+                return weather.current.temperature
+                await client.close()
+            
+            # Get weather    
+            weather = asyncio.run(getWeather(location))
+            
+            # convert weather to Fahrenheit
+            weather_in_celsius = (weather - 32) * (5/9)
+            
+            # Format weather output
+            weather_response = f'The weather is {round(weather_in_celsius, 1)} degrees celsius'
+            
+            # Render response
+            speechEngine(weather_response)
+            
+    else:
+        
+            pass
+        
+
+
+
+
+# Main function
+def main():
+    commandEngine()
+
+
+# Execute main function 
+if __name__ == '__main__':
+    main()
